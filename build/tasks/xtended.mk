14c69468affb1b1b4e5e66abe02375f5fd2ba591
AshutoshSundresh <ashutoshsundresh@gmail.com>
2021-10-12 19:09:29 +0530

    vendor: Add build finished info
    Change-Id: I6b7a45705bb20280ed71a8226fa0d57530c28747
    
    -----
    
    vendor: Change MD5 to SHA256
    
    * Now we generate SHA256 instead of MD5
    
    Change-Id: I464fd2540005599681e8a896d95f35721f61145e
    Signed-off-by: janak <niraulajanak2019@gmail.com>
    Co-Authored-By: xAbdaziz <i@abdaziz.dev>

--- build/tasks/xtended.mk
+++ build/tasks/xtended.mk
@@ -20,12 +20,37 @@ LINEAGE_TARGET_PACKAGE := $(PRODUCT_OUT)/$(LINEAGE_VERSION).zip
 
 SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
 
+CL_PRP="\033[35m"
+CL_RED="\033[31m"
+CL_GRN="\033[32m"
+
 $(LINEAGE_TARGET_PACKAGE): $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(LINEAGE_TARGET_PACKAGE)
 	$(hide) $(SHA256) $(LINEAGE_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(LINEAGE_TARGET_PACKAGE).sha256sum
 	$(hide) ./vendor/everest/common/tools/generate_json_build_info.sh $(LINEAGE_TARGET_PACKAGE)
 	$(hide) ./vendor/everest/common/tools/changelog
-	@echo "Package Complete: $(LINEAGE_TARGET_PACKAGE)" >&2
+	echo -e ${CL_BLD}${CL_RED}"===============================-Package complete-==============================="${CL_RED}
+	echo -e ${CL_RED}"***********************************************" 
+	echo -e ${CL_RED}"                                               "
+	echo -e ${CL_RED}"██╗  ██╗██████╗█████╗█╗   █╗████╗ █████╗████╗  "
+	echo -e ${CL_RED}"╚██╗██╔╝╚═██╔═╝█╔═══╝██╗  █║█╔══█╗█╔═══╝█╔══█╗ "
+	echo -e ${CL_RED}" ╚███╔╝   ██║  ████╗ █╔█╗ █║█║  █║████╗ █║  █║ "
+	echo -e ${CL_RED}" ██╔██╗   ██║  █╔══╝ █║╚█╗█║█║  █║█╔══╝ █║  █║ "
+	echo -e ${CL_RED}"██╔╝ ██╗  ██║  █████╗█║ ╚██║████╔╝█████╗████╔╝ "
+	echo -e ${CL_RED}"╚═╝  ╚═╝  ╚═╝  ╚════╝╚╝  ╚═╝╚═══╝ ╚════╝╚═══╝  "
+	echo -e ${CL_RED}"                                               "
+	echo -e ${CL_RED}"***********************************************"
+	echo -e ${CL_RED}"             Be Ready To Get Xtended           "
+	echo -e ${CL_RED}"***********************************************"
+	echo -e ${CL_RED}"    The Project-Xtended ROM by Team-Xtended    "
+	echo -e ${CL_RED}"***********************************************"
+	echo -e ${CL_BLD}${CL_RED}"===============================-Package complete-==============================="${CL_RED}
+	echo -e ${CL_BLD}${CL_RED}"Zip: "${CL_RED} $(LINEAGE_TARGET_PACKAGE)${CL_RST}
+	echo -e ${CL_BLD}${CL_RED}"SHA256: "${CL_RED}" `cat $(LINEAGE_TARGET_PACKAGE).sha256sum | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_RED}"Size:"${CL_RED}" `du -sh $(LINEAGE_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_RED}"TimeStamp:"${CL_RED}" `cat $(PRODUCT_OUT)/system/build.prop | grep ro.build.date.utc | cut -d'=' -f2 | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_RED}"Integer Value:"${CL_RED}" `wc -c $(LINEAGE_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_RED}"================================================================================"${CL_RED}
 
 .PHONY: xtended
 xtended: $(LINEAGE_TARGET_PACKAGE) $(DEFAULT_GOAL)
